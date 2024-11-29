a040b1ea3fb80c0be8df36f2dda154b90419b338
munyasya21 <munyasyacynthia@gmail.com>
2024-11-29 19:47:17 +0300

    clothing images

--- src/pages/clothing-management.js
+++ src/pages/clothing-management.js
@@ -1,6 +1,9 @@
 import ClothingTable from "./components/ClothingTable.jsx";
 import Image from "next/image.js";
-import img1 from "../assets/images/img1.jpg"
+import dress from "../assets/images/dress.jpg"
+import jeans from "../assets/images/jeans.jpg"
+import skirt from "../assets/images/skirt.jpg"
+import lsneakers from "../assets/images/lsneakers.jpg"
 
 
 
@@ -10,29 +13,29 @@ const ClothingTablePage = () => {
       <ClothingTable/> 
       <div className="pt-20 pl-20 flex gap-10">
         <div>
-       <Image src={img1}
-       alt="blazer"
+       <Image src={dress}
+       alt="dress"
        className="w-64 h-64 rounded-lg"/>
        <p className="pt-5 pl-20">Dresses</p>
        <p className="pt-5 pl-20">$15</p>
        </div>
        <div className="">
-       <Image src={img1}
-       alt="blazer"
+       <Image src={jeans}
+       alt="jeans"
        className="w-64 h-64 rounded-lg"/>
        <p className="pt-5 pl-20">Jeans</p>
        <p className="pt-5 pl-20">$18</p>
        </div>
        <div className="">
-       <Image src={img1}
-       alt="blazer"
+       <Image src={skirt}
+       alt="skirt"
        className="w-64 h-64 rounded-lg"/>
        <p className="pt-5 pl-20">Skirts</p>
        <p className="pt-5 pl-20">$12</p>
        </div>
        <div className="">
-       <Image src={img1}
-       alt="blazer"
+       <Image src={lsneakers}
+       alt="lsneakers"
        className="w-64 h-64 rounded-lg"/>
        <p className="pt-5 pl-20">Sneakers</p>
        <p className="pt-5 pl-20">$25</p>
--- src/pages/mens-clothing.js
+++ src/pages/mens-clothing.js
@@ -1,4 +1,10 @@
 import MensClothing from "../pages/components/MensClothing.jsx";
+import Image from "next/image.js";
+import jeans from "../assets/images/jeans.jpg";
+import Tshirt from "../assets/images/Tshirt.jpg";
+import Blazer from "../assets/images/Blazer.jpg";
+import msneakers from "../assets/images/msneakers.jpg"
+
 
 
 
@@ -8,28 +14,28 @@ const MensClothingPage = () => {
       <MensClothing/> 
       <div className="pt-20 pl-20 flex gap-10">
         <div>
-       <Image src={img1}
+       <Image src={Tshirt}
        alt="blazer"
        className="w-64 h-64 rounded-lg"/>
        <p className="pt-5 pl-20">T-Shirt</p>
        <p className="pt-5 pl-20">$15</p>
        </div>
        <div className="">
-       <Image src={img1}
+       <Image src={jeans}
        alt="blazer"
        className="w-64 h-64 rounded-lg"/>
        <p className="pt-5 pl-20">Jeans</p>
        <p className="pt-5 pl-20">$18</p>
        </div>
        <div className="">
-       <Image src={img1}
+       <Image src={Blazer}
        alt="blazer"
        className="w-64 h-64 rounded-lg"/>
        <p className="pt-5 pl-20">Blazers</p>
        <p className="pt-5 pl-20">$20</p>
        </div>
        <div className="">
-       <Image src={img1}
+       <Image src={msneakers}
        alt="blazer"
        className="w-64 h-64 rounded-lg"/>
        <p className="pt-5 pl-20">Sneakers</p>
